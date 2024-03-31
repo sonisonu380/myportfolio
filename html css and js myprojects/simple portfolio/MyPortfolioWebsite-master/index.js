28bbcf9291d855e000d7e6f34b78b60d8aba2446
sonu soni <160309592+sonisonu380@users.noreply.github.com>
2024-04-01 00:31:08 +0530

    Delete html css and js myprojects directory

--- html css and js myprojects/simple portfolio/MyPortfolioWebsite-master/index.js
+++
@@ -1,8 +0,0 @@
-const menu = document.querySelector(".hamburgermenu");
-const list = document.querySelector(".ul");
-
-menu.addEventListener("click", () => {
-    list.classList.toggle("active")
-})
-
-console.log("hardik");
